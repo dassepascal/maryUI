e3373e03dc30a3541e243fdd9f3f050b6b6273fe
Dasse <pascaldasse56@gmail.com>
2024-11-28 10:34:54 +0100

    language

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+     {{-- Cropper.js --}}
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.js"></script>
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.6.1/cropper.min.css" />
     <title>{{ isset($title) ? $title . ' - ' . config('app.name') : config('app.name') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/livewire/users/edit.blade.php
+++ resources/views/livewire/users/edit.blade.php
@@ -5,28 +5,41 @@ use Mary\Traits\Toast;
 use Livewire\Attributes\Rule;
 use App\Models\User;
 use App\Models\Country;
-
+use Livewire\WithFileUploads;
+use App\Models\Language; 
 
 new class extends Component {
+    use Toast, WithFileUploads;
 
-    use Toast;
-    
     public User $user;
 
-   #[Rule('required')] 
-   public string $name = '';
+    #[Rule('required')]
+    public string $name = '';
 
-   #[Rule('required')] 
-   public string $email = '';
+    #[Rule('required')]
+    public string $email = '';
 
-   // Optional
+    // Optional
     #[Rule('sometimes')]
     public ?int $country_id = null;
 
+    #[Rule('nullable|image|max:1024')]
+    public $photo;
+
+    // Selected languages
+    #[Rule('required')]
+    public array $my_languages = [];
+
+     // Optional 
+    #[Rule('sometimes')]
+    public ?string $bio = null;
 
     public function mount()
     {
         $this->fill($this->user);
+
+        // Fill the selected languages property
+        $this->my_languages = $this->user->languages->pluck('id')->all();
     }
 
     public function save(): void
@@ -37,33 +50,61 @@ new class extends Component {
         //update
         $this->user->update($data);
 
+        // Sync selection
+        $this->user->languages()->sync($this->my_languages);
+
+        // Upload file and save the avatar `url` on User model
+        if ($this->photo) {
+            $url = $this->photo->store('users', 'public');
+            $this->user->update(['avatar' => "/storage/$url"]);
+        }
+
         $this->success('User updated.', redirectTo: '/users');
     }
- 
+
     // We also need this to fill Countries combobox on upcoming form
-    public function with(): array 
+    public function with(): array
     {
         return [
-            'countries' => Country::all()
+            'countries' => Country::all(),
+            'languages' => Language::all(),
         ];
     }
 };
- ?>
+?>
 
 <div>
-   <x-header title="Update {{ $user->name }}" separator>
-   </x-header>
+    <x-header title="Update {{ $user->name }}" separator>
+    </x-header>
+    <div class="grid gap-5 lg:grid-cols-2">
+        <div>
+            <x-form wire:submit="save">
+                <x-file label="Avatar" wire:model="photo" accept="image/png, image/jpeg" crop-after-change>
+                    <img src="{{ $user->avatar ?? '/empty-user.jpg' }}" class="h-40 rounded-lg" />
+                </x-file>
 
-    <x-form wire:submit="save"> 
-        <x-input label="Name" wire:model="name" />
-        <x-input label="Email" wire:model="email" />
-        <x-select label="Country" wire:model="country_id" :options="$countries" placeholder="---" />
- 
-        <x-slot:actions>
-            <x-button label="Cancel" link="/users" />
-            {{-- The important thing here is `type="submit"` --}}
-            {{-- The spinner property is nice! --}}
-            <x-button label="Save" icon="o-paper-airplane" spinner="save" type="submit" class="btn-primary" />
-        </x-slot:actions>
-    </x-form>
+                <x-input label="Name" wire:model="name" />
+                <x-input label="Email" wire:model="email" />
+                <x-select label="Country" wire:model="country_id" :options="$countries" placeholder="---" />
+
+                {{-- Multi selection --}}
+                <x-choices-offline label="My languages" wire:model="my_languages" :options="$languages" searchable />
+
+                <x-editor wire:model="bio" label="Bio" hint="The great biography" /> 
+
+                <x-slot:actions>
+                    <x-button label="Cancel" link="/users" />
+                    {{-- The important thing here is `type="submit"` --}}
+                    {{-- The spinner property is nice! --}}
+                    <x-button label="Save" icon="o-paper-airplane" spinner="save" type="submit"
+                        class="btn-primary" />
+                </x-slot:actions>
+            </x-form>
+        </div>
+        <div>
+            {{-- Get a nice picture from `StorySet` web site --}}
+            <img src="/04-b.jpg" width="300" class="mx-auto" />
+        </div>
+    </div>
+
 </div>
