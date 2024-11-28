e52430feef13c1579d94b5f66253862210d0fba9
Dasse <pascaldasse56@gmail.com>
2024-11-28 13:45:43 +0100

    empty

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,21 @@
 <?php
 
 use Livewire\Volt\Volt;
+use Illuminate\Support\Facades\Route;
+
+
+
+// Users will be redirected to this route if not logged in
+Volt::route('/login', 'login')->name('login');
+ 
+// Define the logout
+Route::get('/logout', function () {
+    auth()->logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+ 
+    return redirect('/');
+});
 
 Volt::route('/', 'index');                          // Home 
 Volt::route('/users', 'users.index');               // User (list) 
